86626d350fb2e8aefdf9ec85f858d3fee147db31
Lethalinjectionx <lethalinjectionx@gmail.com>
2025-12-30 12:12:48 -0500

    implemented RoundUpToPowerOfTwo suggestion

--- AsaApi/Core/Public/Ark/ArkApiUtils.h
+++ AsaApi/Core/Public/Ark/ArkApiUtils.h
@@ -772,9 +772,7 @@ namespace AsaApi
 				return nullptr;
 
 			uint32 alignment = static_cast<uint32>(ss->MinAlignmentField());
-			alignment = FMath::Max<uint32>(alignment, static_cast<uint32>(DEFAULT_ALIGNMENT));
-			alignment = FMath::Max<uint32>(alignment, 1u);
-			alignment = FMath::RoundUpToPowerOfTwo(alignment);
+			alignment = FMath::RoundUpToPowerOfTwo(FMath::Max<uint32>(alignment, 1u));
 			
 			const uint32 unalignedSize = static_cast<uint32>(ss->PropertiesSizeField());
 			if (unalignedSize == 0u)
